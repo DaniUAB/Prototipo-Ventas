89c0bd308873363c4e4695ed8b553abf996e09f2
DaniUAB <daniel.mollinedo@uab.edu.bo>
2026-09-21 16:25:54 -0400

    Dashboard como pantalla de inicio del sistema

--- config/config.php
+++ config/config.php
@@ -6,7 +6,7 @@ define('APP_NAME', 'Sistema de Ventas');
 $base = rtrim(str_replace('\\', '/', dirname($_SERVER['SCRIPT_NAME'] ?? '/')), '/');
 define('BASE_URL', $base . '/');
 
-define('DEFAULT_CONTROLLER', 'venta');
+define('DEFAULT_CONTROLLER', 'dashboard');
 define('DEFAULT_ACTION', 'index');
 
 function url($path = '') {
